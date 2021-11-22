3a9a1d7f1959e620b25f9730678b7ccba2f18b50
Akashleena <akash13leena@gmail.com>
2021-11-22 16:30:50 +0530

    path finding in 2d using navmesh of unity

---
+++ Assets/Pathfinding.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+/*First, you will be making a public array of Transforms simply called points. 
+This array will store the locations of the different points that will be set up in the next section. 
+The object this script is attached to will make its way to each point one by one until you end the game. 
+It will navigate towards these points using the Nav Mesh Agent component, 
+and the code will store a reference of that component in the nav variable.
+Finally, there’s the destPoint integer, which will be used to cycle through the different points laid out for Object */
+
+    public Transform[] points;
+    private NavMeshAgent nav;
+    private int destPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       nav = GetComponent<NavMeshAgent>(); 
+    }
+
+    // Update is called once per frame fixedupdate can be run multiple times per frame
+    void FixedUpdate()
+    {
+        if (!nav.pathPending && nav.remainingDistance < 0.5f) //if you have reached the destination then find the next point to travel to
+  	    GoToNextPoint();
+
+    }
+    //finds the next point for the object to travel to
+    void GoToNextPoint()
+    {
+  	if (points.Length == 0)
+  	    {
+  		return;
+  	    }
+  	nav.destination = points[destPoint].position;
+  	destPoint = (destPoint + 1) % points.Length;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Pathfinding-in-2d-using-Navmesh-of-UNity3D
+
+Unity has a simple way to create a pathfinding system for your objects. This easy process will allow you to get your basic AI off the ground quickly and allow you to create a more complex AI from there. In this example you’ll be creating a navigation mesh that an object will use to make its way to several points that you place in the environment. You will craft an area that you can ‘bake’ a navigation mesh in to start, create an object, and give the object a script that it will use to travel to different waypoints. From there you will set up these waypoints and assign them to the object in order to create a path for your object to follow endlessly.
+
+
